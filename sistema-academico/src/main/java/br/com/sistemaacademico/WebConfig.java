7eff204d74e4e12a299fc719fac0acd89763d8f0
Lucas Ueda <lucas.ueda07@gmail.com>
2025-11-14 16:20:08 -0300

    WebConfig

--- sistema-academico/src/main/java/br/com/sistemaacademico/WebConfig.java
+++ sistema-academico/src/main/java/br/com/sistemaacademico/WebConfig.java
@@ -1,2 +1,14 @@
-package br.com.sistemaacademico;public class WebConfig {
+package br.com.sistemaacademico;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
