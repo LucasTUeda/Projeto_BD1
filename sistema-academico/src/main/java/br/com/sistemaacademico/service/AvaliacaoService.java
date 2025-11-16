ecf70874bdf20442ca52cb74a60e3086be378771
Lucas Ueda <lucas.ueda07@gmail.com>
2025-11-16 00:51:43 -0300

    buscarPendentes e buscarDetalhesAvaliacao

--- sistema-academico/src/main/java/br/com/sistemaacademico/service/AvaliacaoService.java
+++ sistema-academico/src/main/java/br/com/sistemaacademico/service/AvaliacaoService.java
@@ -2,12 +2,16 @@ package br.com.sistemaacademico.service;
 
 import br.com.sistemaacademico.dao.AvaliacaoDAO;
 import br.com.sistemaacademico.dao.AvaliacaoQuestaoDAO;
+import br.com.sistemaacademico.dto.AvaliacaoDetalhadaDTO;
 import br.com.sistemaacademico.model.Avaliacao;
 import br.com.sistemaacademico.dto.AvaliacaoDTO;
+import br.com.sistemaacademico.model.Questao;
+import br.com.sistemaacademico.dao.QuestaoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,11 +19,13 @@ public class AvaliacaoService {
 
     private AvaliacaoDAO avaliacaoDAO;
     private AvaliacaoQuestaoDAO avaliacaoQuestaoDAO;
+    private QuestaoDAO questaoDAO;
 
     @Autowired
-    public AvaliacaoService(AvaliacaoDAO avaliacaoDAO, AvaliacaoQuestaoDAO avaliacaoQuestaoDAO){
+    public AvaliacaoService(AvaliacaoDAO avaliacaoDAO, AvaliacaoQuestaoDAO avaliacaoQuestaoDAO, QuestaoDAO questaoDAO){
         this.avaliacaoDAO = avaliacaoDAO;
         this.avaliacaoQuestaoDAO = avaliacaoQuestaoDAO;
+        this.questaoDAO = questaoDAO;
     }
 
     @Transactional
@@ -37,5 +43,33 @@ public class AvaliacaoService {
         return novoIdAvaliacao;
     }
 
+    public List<Avaliacao> buscarPendentes(int matricula) {
+        // Por enquanto, o service apenas repassa a chamada para o DAO
+        return avaliacaoDAO.findPendentesByMatricula(matricula);
+    }
+
+    public AvaliacaoDetalhadaDTO buscarDetalhesAvaliacao(int idAvaliacao) {
+
+        // 1. Busca a avaliação principal (título, data...)
+        Avaliacao avaliacao = avaliacaoDAO.buscarPorId(idAvaliacao);
+        if (avaliacao == null) {
+            return null; // Ou jogue uma exceção
+        }
+
+        // 2. Busca a lista de IDs de questões (ex: [1, 3, 5])
+        List<Integer> idsQuestoes = avaliacaoQuestaoDAO.findQuestaoIdsByAvaliacaoId(idAvaliacao);
+
+        // 3. Busca cada objeto Questão completo
+        List<Questao> questoesCompletas = new ArrayList<>();
+        for (int idQuestao : idsQuestoes) {
+            Questao questao = questaoDAO.buscarPorId(idQuestao); // Usa o DAO que já tínhamos
+            if (questao != null) {
+                questoesCompletas.add(questao);
+            }
+        }
+
+        // 4. Monta e retorna o DTO de resposta
+        return new AvaliacaoDetalhadaDTO(avaliacao, questoesCompletas);
+    }
 }
 
