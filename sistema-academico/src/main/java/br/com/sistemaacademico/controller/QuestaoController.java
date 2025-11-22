2d7a4f16d2123a8c179535a100d5331976e3c6b3
Lucas Ueda <lucas.ueda07@gmail.com>
2025-11-21 23:17:12 -0300

    Inserção @CrossOrigin(*)

--- sistema-academico/src/main/java/br/com/sistemaacademico/controller/QuestaoController.java
+++ sistema-academico/src/main/java/br/com/sistemaacademico/controller/QuestaoController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/questoes")
+@CrossOrigin("*")
 public class QuestaoController {
     private final QuestaoDAO questaoDAO;
 
--- sistema-academico/src/main/java/br/com/sistemaacademico/controller/RespostaAlunoController.java
+++ sistema-academico/src/main/java/br/com/sistemaacademico/controller/RespostaAlunoController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/respostas")
+@CrossOrigin("*")
 public class RespostaAlunoController {
     private RespostaAlunoService respostaAlunoService;
 
