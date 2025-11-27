df1b72d385cd1f47b1f6828f873b856992c7d1bc
Lucas Ueda <lucas.ueda07@gmail.com>
2025-11-27 15:44:46 -0300

    html do relatório de desempenho

--- frontend-sistema-academico/src/TelaRelatorios.js
+++ frontend-sistema-academico/src/TelaRelatorios.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react';
 
 function TelaRelatorios(){
+
+    // ESTADOS DO RANKING
     const [ranking, setRanking] = useState([]);
     const [idDisciplina, setIdDisciplina] = useState(1); // Começa com a Disciplina 1
     
+    // ESTADOS DO DESEMPENHO
+    const [matriculaAnalise, setMatriculaAnalise] = useState('');
+    const [desempenho, setDesempenho] = useState(null);
+
     useEffect(() => {
         carregarRanking();
     }, [idDisciplina]);
@@ -13,12 +19,23 @@ function TelaRelatorios(){
             .then(res => res.json())
             .then(dados => setRanking(dados))
             .catch(err => console.error("Erro ao carregar ranking:", err));       
-    }
+    };
+
+    const buscarDesempenho = (e) => {
+        e.preventDefault();
+        if(!matriculaAnalise) return;
+
+        fetch(`http://localhost:8080/api/relatorios/desempenho/disciplina/${idDisciplina}/aluno/${matriculaAnalise}`)
+            .then(res => res.json())
+            .then(dados => setDesempenho(dados))
+            .catch(err => alert("Erro ao buscar desempenho. Verifique a matricula"));
+    };
 
     return (
         <div style={{ padding: '20px', maxWidth: '800px', margin: 'auto' }}>
             <h2>📊 Relatórios de Desempenho</h2>
 
+            {/* ----- RELATORIO - RANKING -------*/}  
             <div style={{ marginTop: '20px', border: '1px solid #ddd', padding: '15px', borderRadius: '8px' }}>
                 <h3>🏆 Ranking Geral - Disciplina {idDisciplina} </h3>
 
@@ -50,6 +67,41 @@ function TelaRelatorios(){
                     </table>
                 )} 
             </div>
+
+            {/* ----- RELATORIO - DESEMPENHO -------*/}  
+            <div>
+                <h3>📈 Análise de desempenho (comparativo)</h3>
+                <p>Compare a média de um aluno com a média geral da turma</p>
+
+                <form onSubmit={buscarDesempenho}>
+                    <input
+                        type="number"
+                        placeholder="Matrícula do aluno (ex: 2025001)"
+                        value={matriculaAnalise}
+                        onChange={e => setMatriculaAnalise(e.target.value)}
+                    />
+                    <button type="submit">Comparar</button>
+                </form>
+
+                {desempenho && (
+                    <div>
+                        {/* Card do Aluno */}
+                        <div>
+                            <h4>Média do Aluno</h4>
+                            <h1>{desempenho.mediaAluno ? desempenho.mediaAluno.toFixed(1) : '-'}</h1>
+                            <small>Em todas as provas</small>
+                        </div>
+
+                        {/* Card da Turma */}
+                        <div>
+                            <h4>Média da Turma</h4>
+                            <h1>{desempenho.mediaTurma ? desempenho.mediaTurma.toFixed(1) : '-'}</h1>
+                            <small>Média geral da disciplina</small>
+                        </div>
+                    </div>
+                )}
+            </div>
+
         </div>
     );
 
